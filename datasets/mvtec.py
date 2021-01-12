98947508fe5b98ce9f649c8f7e641d4086d03d16
Daisuke Takahashi <daisuke@hacarus.com>
2021-01-12 10:18:37 +0900

    modify

--- datasets/mvtec.py
+++ datasets/mvtec.py
@@ -10,7 +10,7 @@ from torchvision import transforms as T
 
 
 # URL = 'ftp://guest:GU.205dldo@ftp.softronics.ch/mvtec_anomaly_detection/mvtec_anomaly_detection.tar.xz'
-CLASS_NAMES = ['bottle', 'bottle-small', 'cable', 'capsule', 'carpet', 'grid',
+CLASS_NAMES = ['liberaware', 'bottle', 'bottle-small', 'cable', 'capsule', 'carpet', 'grid',
                'hazelnut', 'leather', 'metal_nut', 'pill', 'screw',
                'tile', 'toothbrush', 'transistor', 'wood', 'zipper']
 
@@ -30,7 +30,7 @@ class MVTecDataset(Dataset):
         # self.download()
 
         # load dataset
-        self.x, self.y, self.mask = self.load_dataset_folder()
+        self.x, self.y = self.load_dataset_folder()
 
         # set transforms
         self.transform_x = T.Compose([T.Resize(resize, Image.ANTIALIAS),
@@ -43,25 +43,19 @@ class MVTecDataset(Dataset):
                                          T.ToTensor()])
 
     def __getitem__(self, idx):
-        x, y, mask = self.x[idx], self.y[idx], self.mask[idx]
+        x, y = self.x[idx], self.y[idx]
 
         x = Image.open(x).convert('RGB')
         x = self.transform_x(x)
 
-        if y == 0:
-            mask = torch.zeros([1, self.cropsize, self.cropsize])
-        else:
-            mask = Image.open(mask)
-            mask = self.transform_mask(mask)
-
-        return x, y, mask
+        return x, y
 
     def __len__(self):
         return len(self.x)
 
     def load_dataset_folder(self):
         phase = 'train' if self.is_train else 'test'
-        x, y, mask = [], [], []
+        x, y = [], []
 
         img_dir = os.path.join(self.dataset_path, self.class_name, phase)
         gt_dir = os.path.join(self.dataset_path, self.class_name, 'ground_truth')
@@ -75,24 +69,18 @@ class MVTecDataset(Dataset):
                 continue
             img_fpath_list = sorted([os.path.join(img_type_dir, f)
                                      for f in os.listdir(img_type_dir)
-                                     if f.endswith('.png')])
+                                     if f.endswith('.jpg')])
             x.extend(img_fpath_list)
 
             # load gt labels
             if img_type == 'good':
                 y.extend([0] * len(img_fpath_list))
-                mask.extend([None] * len(img_fpath_list))
             else:
                 y.extend([1] * len(img_fpath_list))
-                gt_type_dir = os.path.join(gt_dir, img_type)
-                img_fname_list = [os.path.splitext(os.path.basename(f))[0] for f in img_fpath_list]
-                gt_fpath_list = [os.path.join(gt_type_dir, img_fname + '_mask.png')
-                                 for img_fname in img_fname_list]
-                mask.extend(gt_fpath_list)
 
         assert len(x) == len(y), 'number of x and y should be same'
 
-        return list(x), list(y), list(mask)
+        return list(x), list(y)
 
 #     def download(self):
 #         """Download dataset if not exist"""
